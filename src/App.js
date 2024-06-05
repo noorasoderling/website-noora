6c20f7679e52217bc937113654e5cbf2c9203d76
nooratieto <91177925+nooratieto@users.noreply.github.com>
2024-06-05 18:19:06 +0300

    added router functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nooran kotisivut</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -8,6 +9,9 @@ function Navbar() {
             <Link to="/" className="navbar-logo">
                 TRVL
             </Link>
+            <div className='menu-icon'>
+                
+            </div>
         </div>
     </nav>
     </>
