ea010968399852f1cac374ef633e5b7446f40a1e
nooratieto <91177925+nooratieto@users.noreply.github.com>
2024-06-06 15:08:54 +0300

    vikat muutokset navbariin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faDove, faBars, faX} from '@fortawesome/free-solid-svg-icons';
@@ -20,13 +20,17 @@ function Navbar() {
     }
   };
 
+useEffect(() => {
+  showButton();
+}, []);
+
 window.addEventListener('resize', showButton)
 
   return (
     <>
     <nav className="navbar">
         <div className='navbar-container'>
-            <Link to="/" className="navbar-logo">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             <FontAwesomeIcon icon={faDove} />  TRVL
             </Link>
             <div className='menu-icon' onClick={handleClick}>
