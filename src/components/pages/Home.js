660a89d966b94a6b48b606c1d5ea88086b7669cf
nooratieto <91177925+nooratieto@users.noreply.github.com>
2024-06-11 13:56:29 +0300

    Created a footer section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
-
+import Footer from '../Footer';
 
 
 function Home () {
@@ -10,6 +10,7 @@ function Home () {
         <>
         <HeroSection />
         <Cards />
+        <Footer />
         </>
     );
 }
