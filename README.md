8c6f96fb374a8d2a5d9ea7689e7e91cfc869b65b
nooratieto <91177925+nooratieto@users.noreply.github.com>
2024-06-28 10:24:08 +0300

    SVG pictures added and their icon pages and a folder. Added stylingthrough props to them. Also hover/focus styling added to make this accessible as well.

--- README.md
+++ README.md
@@ -68,3 +68,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### OWN NOTES
+
+Themes:
+Dark: #000629;
+Cobalt Blue: #0020D3;
+White: #f3f3f4;
+Peachy Red: ff4529;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,52 +1,19 @@
 import React from 'react';
 import '../App.css';
-import { Link } from 'react-router-dom';
 import './HeroSection.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faInfo, faCodeBranch} from '@fortawesome/free-solid-svg-icons';
-
-
+import SocialMediaSection from './SocialMediaSection.js';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <div className='hero-text-container'>
-      <span className='divider'></span>
-        <h1>Front End Developer</h1>
-        <p>Crafting Digital Experiences, Driving Business Growth</p>
+        <span className='divider'></span>
+          <h1>Front End Developer</h1>
+          <p>Crafting Digital Experiences, Driving Business Growth</p>
+      </div>
+      <SocialMediaSection />
+      <img src="/images/IMG_9247.jpg" alt="Outoors author posing in autumn scenery"></img>
     </div>
-    <img src="/images/IMG_9247.jpg" alt="Outoors author posing in autumn scenery"></img>
-    <div className='hero-socials-container'>
-    <section className='hero-social-media'>
-        <div className='hero-social-media-wrap'>
-            <div>
-                <Link to='/' className='hero-social-logo'>
-                Socials: 
-                </Link>
-                <small className='hero-social-icons'>
-                    <Link 
-                    className='hero-social-icon-link linkedin'
-                    to='/'
-                    target='_blank'
-                    aria-label='LinkedIn'
-                    >
-                        <FontAwesomeIcon icon={faInfo} />
-                    </Link>
-                    <Link 
-                    className='hero-social-icon-link github'
-                    to='/'
-                    target='_blank'
-                    aria-label='Github'
-                    >
-                        <FontAwesomeIcon icon={faCodeBranch} />
-                    </Link>
-                </small>
-            </div>
-        </div>
-      </section>
-
-    </div>
-  </div>
   )
 }
 
